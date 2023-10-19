74b1907845ea9094343ee8af74d3b3af9c8e84f9
Wuashinango <juancarloscarro_@hotmail.com>
2023-10-19 11:43:22 -0600

    agregue el json actualizado

--- dbandBack/server.js
+++ dbandBack/server.js
@@ -2,13 +2,47 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
+
+//debuggear errores
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
+app.disable('x-powered-by');
+
+
 app.set('port', port);
 
-server.listen(port, '192.168.1.13' || 'localhost', function(){
+server.listen(3000, '192.168.1.13' || 'localhost', function(){
     console.log('Aplicacion de NodeJS ' + process.pid + ' Iniciada...')
 });
 
+//RUTAS TEST
+app.get('/', (req, res) => { 
+    res.send('Ruta raiz del backend')
+});
+
+app.get('/test', (req, res) => { 
+    res.send('Esta es la ruta del TEST')
+});
 
 
+// ERROR HANDLER
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
+});
+
+module.exports = {
+    app: app
+}
+
+// 200 - ES UN RESPUESTA EXITOSA
+// 404 - SIGNIFICA QUE LA URL NO EXISTE
+// 500 - ERROR INTERNO DEL SERVIDOR
